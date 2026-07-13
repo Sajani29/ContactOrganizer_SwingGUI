e97a36df64dff71c42c9d50213a749d55a2a78b4
Sajani <22cis0309@ms.sab.ac.lk>
2026-07-14 01:21:56 +0530

    adjust layout

--- MainForm.java
+++ MainForm.java
@@ -5,16 +5,19 @@ class MainForm extends JFrame{
 		JLabel imageLabel;
 		JPanel leftPanel;
 		JPanel rightPanel;
-		MainForm(){
+		MainForm(String title){
 			setSize(1000,600);
 			setLocationRelativeTo(null);
 			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			setLayout(new GridLayout(1,2));
-			
 			ImageIcon image = new ImageIcon("assets/images.png");
-			imageLabel = new JLabel(image);
 			leftPanel = new JPanel();
-			leftPanel.setBackground(Color.RED);
+			leftPanel.setLayout(new GridLayout(2,1));
+			JLabel textLabel = new JLabel("iFriend Contact Organizer");
+			
+			imageLabel = new JLabel(image);
+			leftPanel.setBackground(Color.PINK);
+			leftPanel.add(textLabel);
 			leftPanel.add(imageLabel);
 			add(leftPanel);
 			
@@ -28,7 +31,7 @@ class MainForm extends JFrame{
 			add(rightPanel);
 		}
 	public static void main(String[] args){
-		new MainForm().setVisible(true);
+		new MainForm("iFriend Contact Organizer").setVisible(true);
 		
 	}
 	
