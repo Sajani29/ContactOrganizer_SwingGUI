271b870c5864e1d053f4361d46cb06b0fc3f309b
Sajani <22cis0309@ms.sab.ac.lk>
2026-08-13 02:45:01 +0530

    edit

--- AddContact.java
+++ AddContact.java
@@ -3,9 +3,10 @@ import javax.swing.*;
 import java.awt.*;
 
 class AddContact extends JFrame{
+	private JPanel pblDetails;
 	AddContact(){
 		setTitle("Add Contact");
-		setSize(600,600);
+		setSize(400,400);
 		setDefaultCloseOperation(2);
 		setLocationRelativeTo(null);
 		
@@ -18,5 +19,51 @@ class AddContact extends JFrame{
 		lblTopic.setForeground(Color.BLACK);
 		lblTopic.setPreferredSize(new Dimension(100, 50));
 		
+		JLabel lblContactID = new JLabel("Contact ID: ");
+		lblContactID.setFont(new Font("",1,10));
+		JPanel pnlContactID = new JPanel();
+		pnlContactID.add(lblContactID);
+		
+		JLabel lblName = new JLabel("Name :");
+		lblName.setFont(new Font("",1,10));
+		JPanel pnlName = new JPanel();
+		pnlName.add(lblName);
+		
+		JLabel lblNumber = new JLabel("Phone Number : ");
+		lblNumber.setFont(new Font("",1,10));
+		JPanel pnlNumber = new JPanel();
+		pnlNumber.add(lblNumber);
+		
+		JLabel lblCompanyName = new JLabel("Company Name :");
+		lblCompanyName.setFont(new Font("",1,10));
+		JPanel pnlCompanyName = new JPanel();
+		pnlCompanyName.add(lblCompanyName);
+		
+		JLabel lblSalary = new JLabel("Salary :");
+		lblSalary.setFont(new Font("",1,10));
+		JPanel pnlSalary = new JPanel();
+		pnlSalary.add(lblSalary);
+		
+		
+		JLabel lblBOD = new JLabel("B'Day(YYYY-MM-DD):");
+		lblBOD.setFont(new Font("",1,10));
+		JPanel pnlBOD = new JPanel();
+		pnlBOD.add(lblBOD);
+		
+		pblDetails  = new JPanel(new GridLayout(6,1));
+		pblDetails.add(pnlContactID);
+		pblDetails.add(pnlName);
+		pblDetails.add(pnlNumber);
+		pblDetails.add(pnlCompanyName);
+		pblDetails.add(pnlSalary);
+		pblDetails.add(pnlBOD);
+		
+		pblDetails.setBorder(
+				BorderFactory.createEmptyBorder(20, 5, 10, 30)
+			);
+
+			//rightPanel.add(pnlButton);
+
+		add("West",pblDetails);
 	}
 }
